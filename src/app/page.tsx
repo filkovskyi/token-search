b2266e5b6ba47b0b8628b4590fa7e1da5c55af4b
filkovskyi <sfilkovskyi.immersivehouse@gmail.com>
2024-05-01 17:31:23 +0300

    feature-token-data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 "use client";
-import TokenData from "@/components/tokenData/TokenData";
+
 import TokenChart from "@/components/tokenChart/TokenChart";
-import UserData from "@/components/userData/UserData";
+import TokenData from "@/components/tokenData/TokenData";
 
 export default function Home() {
   return (
     <>
-      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-        <TokenData />
-      </div>
       <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
         <TokenChart />
-        <UserData />
+        <TokenData />
       </div>
     </>
   );
